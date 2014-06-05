ef991688f8efdc5b566cbc639f865074fc8b7746
Art Beckman <aabeckman@yahoo.com>
2014-06-05 14:04:24 -0300

    Filled in steps and dummy step definitions

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -22,11 +22,16 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
+  When I check the following ratings: PG, R
+  When I uncheck the following ratings: G, PG-13
   # enter step to "submit" the search form on the homepage
+  And  I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see the following ratings: PG, R
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see the following ratings: G, PG-13
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: PG, R, G, PG-13
+  And  I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,17 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if !Movie.exists?(title: movie[:title])
+        @movie = Movie.create!(movie)
+        # May need this instead
+        # new_movie = Movie.new
+        # new_movie.title = movie[:title].squish
+        # new_movie.rating = movie[:rating].squish
+        # new_movie.release_date = movie[:release_date].squish
+        # new_movie.save!
+    end
   end
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +23,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +34,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+end
+Then /I should (not )?see the following ratings: (.*)/ do |shouldnot, rating_list|
+  # Make sure that all the movies in the app are visible in the table
+#  flunk "Unimplemented"
 end
